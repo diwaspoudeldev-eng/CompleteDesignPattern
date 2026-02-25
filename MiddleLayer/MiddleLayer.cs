1afeba35ca11787bc9efb243c15f42d1ebf091cc
Diwas Poudel <diwaspoudel.dev@gmail.com>
2026-02-25 15:48:45 -0500

    initial

---
+++ MiddleLayer/MiddleLayer.cs
@@ -0,0 +1,40 @@
+﻿namespace MiddleLayer;
+
+public class CustomerBase
+{
+    public string CustomerName { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public virtual void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(CustomerName))
+            throw new Exception("Customer Name is required");
+
+        if (string.IsNullOrWhiteSpace(PhoneNumber))
+            throw new Exception("Phone Number is required");
+    }
+}
+
+public class Customer : CustomerBase
+{
+    public decimal? BillAmount { get; set; }
+    public DateTime? BillDate { get; set; }
+    public string Address { get; set; }
+
+    public override void Validate()
+    {
+        base.Validate();
+        if (BillAmount == null)
+        {
+            throw new Exception("BillAmount is Required");
+        }
+        if (BillDate > DateTime.Now)
+        {
+            throw new Exception("BillDate is Required");
+        }
+    }
+}
+
+public class Lead : CustomerBase
+{
+}
---
+++ WindowsForm/FormCustomer.cs
@@ -0,0 +1,56 @@
+using MiddleLayer;
+
+namespace WindowsForm
+{
+    public partial class FormCustomer : Form
+    {
+        private Customer cust = null;
+        private Lead lead = null;
+
+        public FormCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetCustomer();
+                cust.Validate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        private void SetCustomer()
+        {
+            cust.CustomerName = txtCustomerName.Text;
+            cust.PhoneNumber = txtPhoneNumber.Text;
+            cust.BillDate = Convert.ToDateTime(txtBillingDate.Text);
+            cust.BillAmount = Convert.ToDecimal(txtBillingAmount.Text);
+            cust.Address = txtAddress.Text;
+        }
+
+        private void cmbCustomerType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbCustomerType.Text == "Lead")
+            {
+                lead = new Lead();
+                cust = null;
+            }
+            else if (cmbCustomerType.Text == "Customer")
+            {
+                cust = new Customer();
+                lead = null;
+            }
+        }
+        private void FrmCustomer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
